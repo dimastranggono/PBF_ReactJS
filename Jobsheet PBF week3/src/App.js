8fd6e373ab4d08baf13a22fcd60fe39f66ef4121
dimastranggono12@gmail.com <dimastranggono12@gmail.com>
2021-03-08 05:41:37 +0700

    Done

---
+++ Jobsheet PBF week3/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Footer from './Footer';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Component dari Class App</h1>
+        <Footer judul='Halaman Footer' nama='Aufa' />
+      </div>
+      
+    );
+  }
+}export default App;
---
+++ Jobsheet PBF week3/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import List from './List';
+import Test from './Test';
+import HelloComponent from './component/HelloComponent';
+import Login from './component/Login';
+
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <List />
+    <Test />
+    <HelloComponent />
+    <Login />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+//If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
